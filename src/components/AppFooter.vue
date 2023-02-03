98093345c98934f397d7d7292cd3d731fbd980af
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-02-03 11:51:09 +0100

    Add 1st col footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -23,11 +23,16 @@ export default {
     <div class="container">
       <div class="row">
         <div class="col-6 py-5">
-          <h3>Address</h3>
+          <h5>Address</h5>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>382 NE 191st St # 87394, Miami, FL 33179-3899</li>
+            <li>+1(305)547-9909 (9am - 5pm EST, Monday - Friday)</li>
+            <li>support@maxcoach.com</li>
+            <li><a href="#"><i class="fa-brands fa-square-facebook"></i></a>
+              <a href="#"><i class="fa-brands fa-twitter"></i></a>
+              <a href="#"><i class="fa-brands fa-instagram"></i></a>
+              <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+            </li>
           </ul>
         </div>
         <div class="col-3  py-5"></div>
@@ -42,5 +47,29 @@ export default {
 footer {
   background-color: $bgMainColor;
   height: 400px;
+  
+  h5{
+    color: $textSecondaryColor;
+    font-weight: bold;
+    margin-bottom: 18px;
+    margin-top: 25px;
+
+  }
+
+  ul {
+    padding: 0;
+  }
+
+  li {
+    margin-bottom: 12px;
+    color: $textSecondaryColor;
+    font-size: 12px;
+
+    .fa-brands {
+    color: $textSecondaryColor;
+    margin-right: 20px;
+    font-size: 25px;
+    }
+  }
 }
 </style>
