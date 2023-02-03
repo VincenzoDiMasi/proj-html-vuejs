6d04ffa010031000dd5969ff22bce660e7e360e2
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-02-03 12:42:02 +0100

    Adj Navbar header

--- src/components/NavbarHeader.vue
+++ src/components/NavbarHeader.vue
@@ -46,18 +46,17 @@ export default {
 
 <template>
   <nav>
-    <div class="container ">
-        <ul class="d-flex justify-content-between">
-            <li> 
-                <figure>
+    <div class="container d-flex justify-content-between">
+        <figure>
                 <a href="#"><img src="../assets/img/dark-logo.png" alt=""></a>
-                 </figure>
-            </li>
+        </figure>
+        <ul class="d-flex justify-content-between">
+        
             <li v-for="link in links"><a href="#">{{ link.text }} <i class="fa-solid fa-caret-down"></i></a></li>
             <li><i class="fa-solid fa-cart-shopping"></i></li>
             <li><i class="fa-regular fa-user"></i></li>
-             <li>
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">               
+            <li>
+                <input class="form-control me-2" type="search" placeholder="Search " aria-label="Search ">               
             </li>
             
         </ul>
@@ -80,11 +79,19 @@ nav {
 
     div {
         figure {
+            margin-left: 70px;
             img{
                 height: 25px;
             }
     }
     }
-    
+    li {
+        margin-right: 30px;
+
+        .form-control {
+            bottom: 5px;
+            position: relative;
+        }
+    }
 }
 </style>
